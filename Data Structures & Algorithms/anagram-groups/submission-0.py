c5790ae34da53574d5a2db49d2b7617966665604
Kacper Kurowski <155920339+Kacperkur@users.noreply.github.com>
2026-07-21 09:14:31 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        answer = []
+        seen = set()
+        # word is our anchor, the current word
+        for word in strs:
+            if word in seen:
+                continue
+
+            anagram = []
+            
+            # option is the iterative choice 
+            for option in strs:
+                
+                # if the length of either is not the same then it cannot be an anagram
+                if len(option) != len(word):
+                    continue
+
+                curr_word = {}
+                next_word = {}
+                # for each letter in the word add them to a dictionary
+                for i in range(len(word)):
+                    curr_word[word[i]] = curr_word.get(word[i], 0) + 1
+                    next_word[option[i]] = next_word.get(option[i], 0) + 1              
+                
+                # add the anagram option to the list, and the set of seen words
+                if curr_word == next_word:
+                    anagram.append(option)
+                    seen.add(option)
+
+            # append the anagram to the answer, it expects a list of lists of strings
+            answer.append(anagram)
+        
+                
+        return answer
+                    
